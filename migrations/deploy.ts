daf0f2894ad9271da8090344f760564bc27e16bb
A91y <65825207+A91y@users.noreply.github.com>
2024-08-19 21:44:51 +0530

    chore: Update Anchor.toml and deploy.ts

--- migrations/deploy.ts
+++ migrations/deploy.ts
@@ -3,34 +3,47 @@
 // configured from the workspace's Anchor.toml.
 
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Giftsol } from "../target/types/giftsol";
+import path from "path";
+import fs from "fs";
 
-const program = anchor.workspace.Giftsol as Program<Giftsol>;
+// Load the IDL (Interface Description Language) for the program
+const idl = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "../target/idl/giftsol.json"), "utf8")
+);
 
-module.exports = async function (provider) {
+module.exports = async function (provider: anchor.AnchorProvider) {
   // Configure client to use the provider.
+  console.log("RPC: ", provider.connection.rpcEndpoint);
   anchor.setProvider(provider);
+  console.log("Deploying the program");
 
   // Add your deploy script here.
-
-  const tx = await program.methods
+  const program = new anchor.Program(idl, provider);
+  await program.methods
     .initialize()
     .accountsPartial({
       globalState: await getGlobalStatePda(),
       admin: provider.wallet.publicKey,
       systemProgram: anchor.web3.SystemProgram.programId,
     })
-    .rpc();
+    .rpc()
+    .then((res) => {
+      console.log("Program initialized");
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+      throw new Error("Program initialization failed");
+    });
 
-  console.log("Program initialized");
-  console.log(tx);
+  async function getGlobalStatePda(): Promise<anchor.web3.PublicKey> {
+    console.log("Getting global state PDA");
+    let globalStatePda: anchor.web3.PublicKey;
+    [globalStatePda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("global_state")],
+      program.programId
+    );
+    console.log("Global state PDA: ", globalStatePda.toBase58());
+    return globalStatePda;
+  }
 };
-
-async function getGlobalStatePda(): Promise<anchor.web3.PublicKey> {
-  const [globalStatePda] = await anchor.web3.PublicKey.findProgramAddress(
-    [Buffer.from("global_state")],
-    program.programId
-  );
-  return globalStatePda;
-}
